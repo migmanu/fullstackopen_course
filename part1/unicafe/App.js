dd0f72bd4e33d7d996948ad0870b7aae267aaf7c
José Manuel Migoya Bussolotti <52668755+migmanu@users.noreply.github.com>
2021-04-03 15:13:54 +0200

    Update App.js
    App.js completed up to exercise 1.7 inclusive.

--- part1/unicafe/App.js
+++ part1/unicafe/App.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
+  const [neutral, setNeutral] = useState(0) 
   const [bad, setBad] = useState(0)
   const [total, setTotal] = useState(0)
   const [average, setAverage] = useState(0)
