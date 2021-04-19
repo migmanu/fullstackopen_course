d19a9bfb3b59fc9725dddb90537cbfafde050f3b
José Manuel Migoya Bussolotti <52668755+migmanu@users.noreply.github.com>
2021-04-19 23:25:29 +0200

    the_phonebook_app completed up to exercise 2.9(step4)

--- part2/the_phonebook_app/src/App.js
+++ part2/the_phonebook_app/src/App.js
@@ -2,15 +2,20 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas',
-      phone: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] = useState('')
+  const [ search, setSearch ] = useState('')
+  const [ toShow, setToShow] = useState(persons)
+
 
   const addPerson = (event) => {
-    event.preventDefault()
     let names = persons.map(person => person.name)
+    event.preventDefault()
 
     if (names.some((name) => name === newName)) {
       return window.alert(`${newName} is already added to the phonebook`)
@@ -18,25 +23,45 @@ const App = () => {
 
     const personObject = {
       name: newName,
-      number: newNumber
+      number: newNumber 
     }
     setPersons(persons.concat(personObject))
     setNewName('')
     setNewNumber('')
-    console.log(personObject)
+    setToShow(toShow.concat(personObject))
   }
   
   const handleNewName = (event) => {
-    setNewName(event.target.value)
+    setNewName(event.target.value) 
   }
  
   const handleNewNumber = (event) => {
     setNewNumber(event.target.value)
   }
   
+  const handleSearch = (event) => {
+    setSearch(event.target.value)
+    showElements(event.target.value, persons)
+  }
+
+  const showElements = (search, persons) => {
+    const elementMatchList = []
+
+    persons.forEach(element => {
+      if (element.name.substring(0, search.length).toLowerCase() === search.toLowerCase()) {
+        elementMatchList.push({name: element.name, number: element.number})
+        }
+      }
+    )
+    setToShow(elementMatchList)
+  }
+ 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      filter contacts by name<input value={search}
+      onChange={handleSearch}/>
+      <h2>Add new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} 
@@ -52,10 +77,11 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person =>
-          <li key={person.name}>
-            {person.name} {person.number}
-          </li>)}
+         {toShow.map(entry =>
+          <li key={entry.name}>
+            {entry.name}  {entry.number}
+          </li>
+      )}
       </ul>
     </div>
   )
